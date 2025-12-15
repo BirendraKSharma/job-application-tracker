55899d9af8a4e0fb124d83d1cf349b986580eb6a
Birendra <07bct025@ioepc.edu.np>
2025-12-16 01:46:32 +0545

    Adding readme file and requirements

--- core/core/urls.py
+++ core/core/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'), #for default page
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('jobs/', include('jobs.urls')),
 ]
